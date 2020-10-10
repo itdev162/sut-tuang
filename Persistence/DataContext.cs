f5c90afc7c05697c3fc7ede6de88ac7ba9cf5c6b
johnpiapian <johnpiapian@gmail.com>
2020-10-10 17:16:05 -0500

    Completed Activity 3

--- Persistence/DataContext.cs
+++ Persistence/DataContext.cs
@@ -11,5 +11,14 @@ namespace Persistence
         {
             
         }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Value>().HasData(
+                new Value { Id = 1, Name = "Value1" },
+                new Value { Id = 2, Name = "Value2" },
+                new Value { Id = 3, Name = "Value3" }
+            );
+        }
     }
 }
