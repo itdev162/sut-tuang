15d73bea622dc527e1bb864ccad9a20b9b5ba7b0
johnpiapian <johnpiapian@gmail.com>
2020-10-10 16:07:41 -0500

    Add DataContext.cs

---
+++ Persistence/DataContext.cs
@@ -0,0 +1,15 @@
+using Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Persistence 
+{
+    public class DataContext: DbContext
+    {
+        public DbSet<Value> Values { get; set;}
+
+        public DataContext(DbContextOptions options) : base(options) 
+        {
+            
+        }
+    }
+}
