86b250a7c3f33a1802bd2e134ae2128bd9adc55a
johnpiapian <johnpiapian@gmail.com>
2020-10-11 18:50:32 -0500

    Completed Activity 4

--- API/Startup.cs
+++ API/Startup.cs
@@ -22,12 +22,25 @@ namespace API
             Configuration = configuration;
         }
 
+        private readonly string CorsPolicy = "CorsPolicy";
+
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             //services.AddControllers();
+
+            services.AddCors(opt =>
+            {
+               opt.AddPolicy(CorsPolicy, policybuilder =>
+               {
+                    policybuilder
+                        .WithOrigins("http://localhost:3000")
+                        .AllowAnyHeader()
+                        .AllowAnyMethod();
+               });
+            });
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_3_0);
 
@@ -45,12 +58,23 @@ namespace API
                 app.UseDeveloperExceptionPage();
             }
 
-            // app.UseHttpsRedirection();
+            /*
+            app.UseHttpsRedirection();
 
             app.UseRouting();
 
             app.UseAuthorization();
 
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+            */
+
+            app.UseCors(CorsPolicy);
+            app.UseRouting();
+            app.UseAuthorization();
+            
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,33 @@
+import Axios from 'axios';
 import React from 'react';
 import './App.css';
+import axios from 'axios';
 
 class App extends React.Component {
+
+  state = {
+    values: []
+  }
+
+  componentDidMount(){
+    axios.get('http://localhost:5000/api/WeatherForecast')
+    .then((response) => {
+      this.setState({
+        values: response.data
+      })
+    })
+    .catch((error) => {
+      console.error(`Error fetching data: ${error}`);
+    })
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-headeer">
           Blogbox
         </header>
+        {this.state.values.map((value: any) => <div key={value.date}>{value.summary}</div>)}
       </div>
     );
   }
